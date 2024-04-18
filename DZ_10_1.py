4dec67a371a0bd058cdbc9ef5cb04ef2d5750b3e
KAIIIAPA <158216233+KAIIIAPA@users.noreply.github.com>
2024-04-18 18:18:50 +0300

    Add files via upload

---
+++ DZ_10_1.py
@@ -0,0 +1,31 @@
+from time import sleep
+from threading import Thread
+'''
+Задание:
+1. Напишите программу, которая создает два потока.
+2. Первый поток должен выводить числа от 1 до 10 с интервалом в 1 секунду.
+3. Второй поток должен выводить буквы от 'a' до 'j' с тем же интервалом.
+4. Оба потока должны работать параллельно.
+'''
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+def numbers_func():
+    for i in range(11):
+        print(i)
+        sleep(1)
+def letters_func():
+    for i in letters:
+        print(i, flush=True)
+        sleep(1)
+
+
+num_1 = Thread(target=numbers_func)
+let_1 = Thread(target=letters_func)
+
+num_1.start()
+let_1.start()
+
+
+num_1.join()
+let_1.join()
